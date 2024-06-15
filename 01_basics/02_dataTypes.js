e1cfae1e78831ea0a9e73b51ec86655b8d16cd5c
Chitransh1234 <98686772+Chitransh1234@users.noreply.github.com>
2024-06-15 10:13:46 +0000

    data types in js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"Use strict";// treat all JS code as newer version 
+
+// alert(3+3) //we are using node.js not browser
+
+console.log(3+3)
+//code readability should be high
+console.log("chitransh")
+
+let name="Chitransh"
+let age=18
+let isLoggedIn=false
+let state;
+//  PRIMITIVE DATATYPES
+// number =>2 to power 53
+// bigint
+// string => ""
+// boolean =>true/false
+// null => standalone value
+// undefined => abhi value assign nhi hui hai
+// symbol => Unique
+
+
+
+//OBJECT
+console.log(typeof undefined);//undefined
+console.log(typeof null);// object
+
